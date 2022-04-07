ed903e7cd4aaf5391375856a6f2b6a4ec5bda3df
Joan Rodas <joan@sirvelia.com>
2022-04-07 14:54:34 +0200

    Add plubo routes

--- public/routes.php
+++ public/routes.php
@@ -1,4 +1,6 @@
 <?php
+use PluboRoutes\PluboRoutesProcessor;
+use PluboRoutes\Route;
 
 class PluginPlaceholderRoutes {
 
@@ -8,30 +10,13 @@ class PluginPlaceholderRoutes {
   public function __construct( $plugin_name, $plugin_version ) {
 		$this->plugin_name = $plugin_name;
 		$this->plugin_version = $plugin_version;
-
-		add_action( 'init', array($this, 'add_rewrite_rules') );
-		add_filter( 'query_vars', array($this, 'add_query_vars') );
-		add_action( 'template_include', array($this, 'include_templates') );
+		PluboRoutesProcessor::init();
+		add_filter( 'plubo/routes', array($this, 'add_routes') );
 	}
 
-	public function add_rewrite_rules() {
-		//add_rewrite_rule( 'example/([a-z0-9-]+)[/]?$', 'index.php?example=$matches[1]', 'top' );
-		add_rewrite_rule(
-			'^dashboard/?$',
-			'index.php?s=test',
-			'top'
-		);
-		return;
-	}
-
-	public function add_query_vars( $query_vars ) {
-		// $query_vars[] = 'new_query_var';
-		return $query_vars;
-	}
-
-	public function include_templates( $template ) {
-		// if( get_query_var( 'example' ) ) $template = ;
-		return $template;
+	public function add_routes( $routes ) {
+		$routes[] = new Route('testing', 'testing/{year:number}/{city:word}', 'test');
+		return $routes;
 	}
 
 }
