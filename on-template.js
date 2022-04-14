c0fcc8788db530233f95cfbb9b4a43dc2830dab2
Joan Rodas <joan@sirvelia.com>
2022-04-14 21:38:35 +0200

    Push

--- on-template.js
+++ on-template.js
@@ -57,17 +57,9 @@ writeFileSync('./README.md', newReadme, { encoding: 'utf8' })
 const execSync = require('child_process').execSync;
 
 
-const output = execSync('find ./ -type f -name \'*.php\' | xargs sed -i "s/plugin-placeholder/${PWD##*/}/g"', { encoding: 'utf-8' });  // the default is 'buffer'
+const output = execSync('chmod +x plubo.sh && ./plubo.sh', { encoding: 'utf-8' });  // the default is 'buffer'
 console.log('Output was:\n', output);
 
-const output2 = execSync('find ./ -type f -name \'*.php\' | xargs sed -i "s/PLUGIN_PLACEHOLDER/$(tr \'[a-z]\' \'[A-Z]\' <<< ${PWD##*/})/g"', { encoding: 'utf-8' });  // the default is 'buffer'
-console.log('Output was:\n', output2);
-
-const output3 = execSync('find ./ -type f -name \'*.php\' | xargs sed -i "s/PluginPlaceholder/$(awk \'BEGIN{FS="";RS="-";ORS=""} {$0=toupper(substr($0,1,1)) substr($0,2)} 1\' <<< ${PWD##*/})/g"', { encoding: 'utf-8' });  // the default is 'buffer'
-console.log('Output was:\n', output3);
-
-const output4 = execSync('php -r "rename(\'plugin-placeholder.php\', \'${PWD##*/}.php\');"', { encoding: 'utf-8' });  // the default is 'buffer'
-console.log('Output was:\n', output4);
 
 
 /**
