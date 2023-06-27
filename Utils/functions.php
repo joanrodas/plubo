4fcc9dd743269d4d8288156d5c3e3a5ee0445eae
Joan Rodas <joan@sirvelia.com>
2023-06-27 15:46:53 +0200

    Fix func name

--- Utils/functions.php
+++ Utils/functions.php
@@ -4,7 +4,7 @@ if (!function_exists("pb_script")) {
     function pb_script(string $name, string $path, callable $condition = null, array $dependencies = [], $localize = false)
     {
         PluginPlaceholder\Includes\AssetsLoader::getInstance()
-        ->add_script($name, $path, $condition, $dependencies, $localize);
+        ->addScript($name, $path, $condition, $dependencies, $localize);
     }
 }
 
@@ -12,7 +12,7 @@ if (!function_exists("pb_style")) {
     function pb_style(string $name, string $path, callable $condition = null, array $dependencies = [])
     {
         PluginPlaceholder\Includes\AssetsLoader::getInstance()
-        ->add_style($name, $path, $condition, $dependencies);
+        ->addStyle($name, $path, $condition, $dependencies);
     }
 }
 
@@ -20,7 +20,7 @@ if (!function_exists("pb_admin_script")) {
     function pb_admin_script(string $name, string $path, callable $condition = null, array $dependencies = [], $localize = false)
     {
         PluginPlaceholder\Includes\AssetsLoader::getInstance()
-        ->add_admin_script($name, $path, $condition, $dependencies, $localize);
+        ->addAdminScript($name, $path, $condition, $dependencies, $localize);
     }
 }
 
@@ -28,6 +28,6 @@ if (!function_exists("pb_admin_style")) {
     function pb_admin_style(string $name, string $path, callable $condition = null, array $dependencies = [])
     {
         PluginPlaceholder\Includes\AssetsLoader::getInstance()
-        ->add_admin_style($name, $path, $condition, $dependencies);
+        ->addAdminStyle($name, $path, $condition, $dependencies);
     }
 }
